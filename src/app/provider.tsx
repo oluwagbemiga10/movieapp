fce93ac3a983865d5ce844edfdf9226420748226
oluwagbemiga10 <oyelamigbenga11@gmail.com>
2024-06-18 04:22:47 +0100

    changes made

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import {ThemeProvider} from 'next-themes'
 
-const Providers = ({children}) => {
+const Providers = ({children}:any) => {
   return (
     <ThemeProvider defaultTheme='system'>
         <div className='text-gray-700 dar:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300'>
